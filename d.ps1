5aabd5615b044a03b8fd9dc95686f6dca0c464bc
StirFrySteve <49064880+StirFrySteve@users.noreply.github.com>
2020-11-04 09:27:28 +1000

    Update d.ps1

--- d.ps1
+++ d.ps1
@@ -1,3 +1,3 @@
-wget 'https://i.ytimg.com/vi/lcjERakHru8/hqdefault.jpg' -OutFile c.bmp
+wget 'https://images-na.ssl-images-amazon.com/images/I/71sT894PNkL.jpg' -OutFile c.bmp
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  C:\win\c.bmp /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
